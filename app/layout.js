6bf2bedb911037d10a4435a663f15c3cab966f9a
solomon <solomon.chukwuka28@gmail.com>
2023-09-26 16:21:18 +0100

    create the about page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
-
+import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,9 +14,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        
+        <Providers>
+          {/* Header */}
+          <Header />
 
-        {children}
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
 import Image from "next/image";
-import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
-      {/* Header */}
-      <Header />
       <h1>Home</h1>
     </>
   );
